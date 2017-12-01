a420eb79a8dc3b6d325b78266ee8b710629d6104
Vithal R Bonker <vbonker@asiapac.globalcsc.net>
2017-12-01 22:09:56 +0530

    fixed the compile errors

--- ScriptlessTool/src/app/guiComponents/projectLab/automationscript/params/pathparams/pathparams.component.ts
+++ ScriptlessTool/src/app/guiComponents/projectLab/automationscript/params/pathparams/pathparams.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PathparamsComponent implements OnInit {
 
+  componentReference: any;
+
   constructor() { }
 
   ngOnInit() {
   }
-
-  var componentReference:any;
 
   removeComponent(){
     this.componentReference.destroy();
